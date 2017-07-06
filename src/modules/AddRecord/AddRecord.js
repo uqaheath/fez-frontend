ffbcaa7e5252e53d4cd0bdb78c7a2dc5c91e9898
Vishal Asai <asai_vishal@yahoo.com>
2017-07-06 14:40:41 +1000

    updated AddRecord component

--- src/modules/AddRecord/AddRecord.js
+++ src/modules/AddRecord/AddRecord.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import {Card, CardText} from 'material-ui/Card';
+import {locale} from 'config';
+import AddRecordStepper from '../../components/AddRecordStepper';
+// import AddRecordStepContentContainer from '../../containers/AddRecordStepContentContainer';
+import AddRecordStepContent from '../../components/AddRecordStepContent';
 
-export default function AddRecord() {
-    return (
-        <div className="layout-fill">
-            <h1 className="page-title display-1">Add a journal article</h1>
-            <Card className="layout-card">
-                <CardText className="body-1">
-                    <br />
-                    <div>
-                        <p>
-                            Placeholder for a form
-                        </p>
-                    </div>
-                </CardText>
-            </Card>
-        </div>
-    );
+export default class AddRecord extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="layout-fill">
+                <h1 className="page-title display-1">{ locale.pages.addRecord.title }</h1>
+                <AddRecordStepper />
+                <AddRecordStepContent />
+            </div>
+        );
+    }
 }
