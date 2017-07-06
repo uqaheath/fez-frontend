247e9822e4b7cdb9c38d29b55a2b6807be9ce18c
Vishal Asai <asai_vishal@yahoo.com>
2017-07-06 14:38:14 +1000

    Added some locales for Search publication

--- src/config/locale.js
+++ src/config/locale.js
@@ -5,6 +5,10 @@ export default {
         title: 'UQ eSpace',
         logo: 'https://static.uq.net.au/v1/logos/corporate/uq-logo-white.svg'
     },
+    authentication: {
+        signInText: 'Log in',
+        signOutText: 'Log out'
+    },
     menu: {
         myDashboard: {
             primaryText: 'My dashboard'
@@ -106,6 +110,66 @@ export default {
                     </div>
                 ),
                 button: 'OK'
+            }
+        },
+        addRecord: {
+            stepper: {
+                steps: [
+                    {
+                        label: 'Search for your publication'
+                    },
+                    {
+                        label: 'Search results'
+                    },
+                    {
+                        label: 'Add your publication',
+                    }
+                ],
+                defaultErrorMessage: 'Error'
+            },
+            title: 'Add missing record',
+            searchForPublication: {
+                title: 'Search for your publication',
+                explanationText: 'Enter either the publication DOI (e.g. 10.1163/9789004326828), Pubmed Id (e.g. 28131963) or the title of the publication. This will allow us to check whether the record is already in eSpace or is available from another source.',
+                defaultSearchFieldLabel: 'Enter DOI, Pubmed Id or Title',
+                defaultButtonLabel: 'Search',
+                errorMsg: 'Please enter a valid publication DOI (e.g. 10.1163/9789004326828), Pubmed ID (e.g. 28131963) or the title (min 10 characters) of the publication',
+                help: {
+                    title: 'Search for your publication',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet ac risus et blandit. Vivamus varius ornare metus vitae sagittis. Donec erat urna, interdum vitae faucibus a, tempus eu orci. Aenean venenatis lacus eu sapien dignissim, non rhoncus dolor facilisis. Donec finibus tristique nunc nec facilisis. Pellentesque luctus libero faucibus ex mattis, vitae commodo nunc vehicula. Nam nec porttitor sapien. Sed rutrum, mauris id luctus eleifend, eros lectus auctor nibh, a eleifend est est eu nunc.'
+                },
+                buttonLabelVariants: {
+                    doi: 'DOI Search',
+                    pubmed: 'Pubmed Id Search',
+                    title: 'Title Search'
+                },
+                inlineLoader: {
+                    message: 'Loading ...'
+                },
+            },
+            searchResults: {
+                title: 'Possible matches found',
+                explanationText: 'Top [noOfResults] matches displayed below. To refine your search and narrow down results, please click the "search again" button below - or create a new record.',
+                claimRecordBtnLabel: 'Claim This Record',
+                help: {
+                    title: 'Possible matches found',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet ac risus et blandit. Vivamus varius ornare metus vitae sagittis. Donec erat urna, interdum vitae faucibus a, tempus eu orci. Aenean venenatis lacus eu sapien dignissim, non rhoncus dolor facilisis. Donec finibus tristique nunc nec facilisis. Pellentesque luctus libero faucibus ex mattis, vitae commodo nunc vehicula. Nam nec porttitor sapien. Sed rutrum, mauris id luctus eleifend, eros lectus auctor nibh, a eleifend est est eu nunc.'
+                },
+                limit: 5
+            },
+            noMatchingRecords: {
+                title: 'No matching records?',
+                explanationText: 'Refine your search and narrow down results, or create a new eSpace record for your publication.',
+                searchAgainBtnLabel: 'Search again',
+                addPublicationBtnLabel: 'Add a new publication',
+                help: {
+                    title: 'No matchings records?',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet ac risus et blandit. Vivamus varius ornare metus vitae sagittis. Donec erat urna, interdum vitae faucibus a, tempus eu orci. Aenean venenatis lacus eu sapien dignissim, non rhoncus dolor facilisis. Donec finibus tristique nunc nec facilisis. Pellentesque luctus libero faucibus ex mattis, vitae commodo nunc vehicula. Nam nec porttitor sapien. Sed rutrum, mauris id luctus eleifend, eros lectus auctor nibh, a eleifend est est eu nunc.'
+                },
+                defaultProps: {
+                    searchAgainBtnLabel: 'Search again?',
+                    addPublicationBtnLabel: 'Add new publication'
+                }
             }
         }
     }
