d172a25a91bc10a1fa694bd089a5c2b9f7b4f888
Vishal Asai <asai_vishal@yahoo.com>
2017-07-06 14:55:34 +1000

    Added providers index

---
+++ src/providers/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by uqvasai on 3/07/2017.
+ */
+import publicationsearch from './publicationsearch';
+import stepper from './stepper';
+
+export default {
+    stepper,
+    publicationsearch
+};
