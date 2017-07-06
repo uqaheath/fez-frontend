48d3a1c85e274b1066bebeaf28acf69fe755ca9d
Vishal Asai <asai_vishal@yahoo.com>
2017-07-06 14:39:36 +1000

    Added query string params helper

---
+++ src/helpers/repository-query-string.js
@@ -0,0 +1,20 @@
+/**
+ * Created by uqvasai on 5/07/2017.
+ */
+import { isDOIValue, isPartialDOIValue, isPubMedValue } from '../providers/validator';
+
+export const getQueryString = (value) => {
+    if (value) {
+        if (isPartialDOIValue(value) || isDOIValue(value)) {
+            return `doi=${value}`;
+        } else if (isPubMedValue(value)) {
+            return `pub_med_id=${value}`;
+        } else if (/^\D.*/i.test(value) || value.trim().length > 5) {
+            /**
+             * @todo Move display type to config or make it configurable
+             */
+            return `source=wos&rek_display_type=179&title=${value}`;
+        }
+    }
+    return '';
+};
