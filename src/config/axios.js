f2c34e83d9378cf1c9e34be69b7e6498105546cd
Vishal Asai <asai_vishal@yahoo.com>
2017-07-06 14:37:22 +1000

    token cancel function

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,10 +2,18 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 import {API_URL, SESSION_COOKIE_NAME, TOKEN_NAME} from './general';
 
-const api = axios.create({
+export const api = axios.create({
     baseURL: API_URL
 });
 
+// need to generate a new token for each request otherwise if you try a new request with the old token,
+// axios will appear to cancel your request automatically
+export const generateCancelToken = () => {
+    const CancelToken = axios.CancelToken;
+    return CancelToken.source();
+};
+
 api.defaults.headers.common[TOKEN_NAME] = Cookies.get(SESSION_COOKIE_NAME);
 
-export default api;
+// needed for cancelling requests and the instance created does not have this method
+api.isCancel = axios.isCancel;
