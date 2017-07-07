45b9f681c2624453c15c5667ce3d6bc9ba6ac21b
Vishal Asai <asai_vishal@yahoo.com>
2017-07-07 13:05:29 +1000

    Add reducers from providers

--- src/reducer.js
+++ src/reducer.js
@@ -1,18 +1,20 @@
-import {combineReducers} from 'redux-immutable';
-
-// import {reducer as formReducer} from 'redux-form/immutable';
+import { combineReducers } from 'redux-immutable';
 
 // Load reducers
-import {appReducer} from 'modules/App';
-import {reducer as dashboardReducer} from 'modules/Dashboard';
-import {reducer as formReducer} from 'redux-form/immutable';
-import {helpDrawerReducer} from 'uqlibrary-react-toolbox';
+import { appReducer } from 'modules/App';
+import { reducer as dashboardReducer } from 'modules/Dashboard';
+import { reducer as formReducer } from 'redux-form/immutable';
+import { helpDrawerReducer } from 'uqlibrary-react-toolbox';
+import stepper from 'providers/stepper';
+import publicationsearch from 'providers/publicationsearch';
 
 const rootReducer = combineReducers({
     app: appReducer,
     form: formReducer,
     dashboard: dashboardReducer,
-    helpDrawer: helpDrawerReducer
+    helpDrawer: helpDrawerReducer,
+    ...stepper.reducers,
+    ...publicationsearch.reducers
 });
 
 export default rootReducer;
