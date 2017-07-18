ac0b405f23a3e90db9049dafc996ab6edec13e61
Vishal Asai <asai_vishal@yahoo.com>
2017-07-18 15:25:09 +1000

    updating search results container

--- src/modules/SearchResults/containers/SearchResults.js
+++ src/modules/SearchResults/containers/SearchResults.js
@@ -1,8 +1,22 @@
-import {reduxForm} from 'redux-form/immutable';
+import { reduxForm } from 'redux-form/immutable';
+import { connect } from 'react-redux';
+import { loadResultsList } from '../actions';
+
 import SearchResults from '../components/SearchResults';
 
-const SearchResultsContainer = reduxForm({
+const SearchResultsForm = reduxForm({
     form: 'SearchResultsForm'
 })(SearchResults);
 
-export default SearchResultsContainer;
+export default connect((state) => {
+    return {
+        searchText: state.get('searchResult').get('searchPublicationText'),
+        dataSource: state.get('searchResult').get('searchResultsList'),
+        loadingSearch: state.get('searchResult').get('loadingSearch'),
+        loadingMoreSearch: state.get('searchResult').get('loadingMoreSearch')
+    };
+}, dispatch => {
+    return {
+        performSearch: searchText => dispatch(loadResultsList(searchText))
+    };
+})(SearchResultsForm);
