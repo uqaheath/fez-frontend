5a49a26f7772ce845e8f5c9b4e11ae3219e20533
Vishal Asai <asai_vishal@yahoo.com>
2017-07-18 15:20:47 +1000

    added logical function for abandoning search

--- src/modules/AddRecord/actions.js
+++ src/modules/AddRecord/actions.js
@@ -1,5 +1,6 @@
 // Repositories
 import {loadPublicationTypesData} from 'repositories/publicationTypes';
+import { CLEAR_SEARCH_RESULTS } from '../SearchResults/constants';
 
 // Types
 export const PUBLICATION_TYPES_LOADING = 'PUBLICATION_TYPES_LOADING';
@@ -7,7 +8,6 @@ export const PUBLICATION_TYPES_LOADED = 'PUBLICATION_TYPES_LOADED';
 export const ADD_RECORD_STEPPER_INDEX_INCREASED = 'ADD_RECORD_STEPPER_INDEX_INCREASED';
 export const ADD_RECORD_STEPPER_INDEX_DECREASED = 'ADD_RECORD_STEPPER_INDEX_DECREASED';
 export const ADD_RECORD_STEPPER_INDEX_RESET = 'ADD_RECORD_STEPPER_INDEX_RESET';
-
 
 /**
  * Loads the publication types into the application
@@ -62,3 +62,9 @@ export function resetStepper() {
     };
 }
 
+export function abandonSearch() {
+    return dispatch => {
+        dispatch({ type: ADD_RECORD_STEPPER_INDEX_RESET });
+        dispatch({ type: CLEAR_SEARCH_RESULTS });
+    };
+}
