dbbf11a9c9329b0d3c7b3ebcaf433a222a3b4a51
Vishal Asai <asai_vishal@yahoo.com>
2017-07-18 15:23:38 +1000

    updating publication search reducer

--- src/modules/Forms/PublicationSearch/reducer.js
+++ src/modules/Forms/PublicationSearch/reducer.js
@@ -1,29 +1,17 @@
-import Immutable from 'immutable';
+import { combineReducers } from 'redux-immutable';
+import { SEARCH_INITIATED } from '../../SearchResults/constants';
+import { convertToQueryString } from '../../../helpers/search';
 
-import {DOI_SEARCH_COMPLETED, PUBMED_SEARCH_COMPLETED, TITLE_SEARCH_COMPLETED, SEARCH_RESULTS_CLEARED} from './actions';
-import {TITLE_SEARCH_LOADING, PUBMED_SEARCH_LOADING, DOI_SEARCH_LOADING} from './actions';
-
-// Immutable state
-export const initialState = Immutable.fromJS({
-    searchResultsList: {},
-    loadingSearch: false
-});
-
-const publicationSearchReducer = (state = initialState, action) => {
+function publicationSearchText(state = '', action) {
     switch (action.type) {
-        case DOI_SEARCH_LOADING:
-        case PUBMED_SEARCH_LOADING:
-        case TITLE_SEARCH_LOADING:
-            return state.set('loadingSearch', true);
-        case DOI_SEARCH_COMPLETED:
-        case PUBMED_SEARCH_COMPLETED:
-        case TITLE_SEARCH_COMPLETED:
-            return state.set('searchResultsList', Immutable.fromJS(action.payload)).set('loadingSearch', false);
-        case SEARCH_RESULTS_CLEARED:
-            return state.set('searchResultsList', Immutable.fromJS({}));
+        case SEARCH_INITIATED:
+            return convertToQueryString(action.payload);
         default:
             return state;
     }
-};
+}
+const publicationSearchReducer = combineReducers({
+    publicationSearchText
+});
 
 export default publicationSearchReducer;
