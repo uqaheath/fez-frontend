f2d4cf7c31e76a4113e10865a1c39d02c0d09807
Vishal Asai <asai_vishal@yahoo.com>
2017-07-18 15:23:58 +1000

    updating publication search form component

--- src/modules/Forms/PublicationSearch/components/PublicationSearchForm.js
+++ src/modules/Forms/PublicationSearch/components/PublicationSearchForm.js
@@ -4,12 +4,9 @@ import {Field} from 'redux-form/immutable';
 import PropTypes from 'prop-types';
 import {HelpIcon, TextField} from 'uqlibrary-react-toolbox';
 import RaisedButton from 'material-ui/RaisedButton';
-import {isDOIValue, isPubMedValue} from '../validator';
 import {locale} from 'config';
 
 import './PublicationSearchForm.scss';
-
-const JOURNAL_ARTICLE = 179;
 
 export default class PublicationSearchForm extends Component {
 
@@ -18,9 +15,7 @@ export default class PublicationSearchForm extends Component {
         explanationText: PropTypes.string.isRequired,
         pristine: PropTypes.bool,
         handleSubmit: PropTypes.func,
-        loadDoiResultsList: PropTypes.func,
-        loadPubmedResultsList: PropTypes.func,
-        loadTitleResultsList: PropTypes.func,
+        onSearchSubmit: PropTypes.func,
         formValues: PropTypes.object,
         help: PropTypes.object,
         searchTitleField: PropTypes.string
@@ -31,23 +26,15 @@ export default class PublicationSearchForm extends Component {
     }
 
     performSearch = (event) => {
-        // TODO: fix form submit, all data fetching should be done outside of the form
         // workaround: if user clicks Enter on search field OR search button, search is initiated and form is submitted
         if (event && event.key && event.key !== 'Enter') {
             return;
         }
 
         const { formValues } = this.props;
-        const fieldValue = formValues.get('doiSearch');
+        const fieldValue = formValues.get('publicationSearchText');
 
-        if (isDOIValue(fieldValue)) {
-            this.props.loadDoiResultsList(fieldValue);
-        } else if (isPubMedValue(fieldValue)) {
-            this.props.loadPubmedResultsList(fieldValue);
-        } else {
-            // Pass fieldValue map through to the form component
-            this.props.loadTitleResultsList(JOURNAL_ARTICLE, fieldValue);
-        }
+        this.props.onSearchSubmit(fieldValue);
     };
 
     render() {
@@ -76,7 +63,7 @@ export default class PublicationSearchForm extends Component {
                     <CardText className="body-1">
                         <div>{explanationText}</div>
                         <Field component={TextField}
-                               name="doiSearch"
+                               name="publicationSearchText"
                                fullWidth
                                floatingLabelText={searchForPublicationInformation.defaultSearchFieldLabel}
                                autoComplete="off"
