69da73d8b712f4e6b0f8005750819557c9c01013
Vishal Asai <asai_vishal@yahoo.com>
2017-07-20 12:08:15 +1000

    Claim publication reducer simplified, reused searchResultsList reducer using Higher Order reducer function

--- src/modules/ClaimPublication/reducer.js
+++ src/modules/ClaimPublication/reducer.js
@@ -1,17 +1,18 @@
+/*
 import Immutable from 'immutable';
+*/
 import {
     CLAIM_SUBMITTING,
     CLAIM_SUBMITTED,
     CLAIM_SUBMIT_FAILED,
     CLAIM_SUBMIT_RESET,
     PUBLICATION_SELECTED,
-    PUBLICATION_SELECTED_CLEARED,
-    PUBLICATION_RESULTS_CLEARED,
-    USERS_PUBLICATIONS_LOADED,
-    USERS_PUBLICATIONS_LOADING,
-    USER_PUBLICATIONS_MARKED_NOT_MINE_COMPLETED} from './actions';
-import {locale} from 'config';
-import {actionTypes} from 'redux-form';
+    PUBLICATION_SELECTED_CLEARED
+} from './actions';
+
+import { combineReducers } from 'redux-immutable';
+import { createSearchResultsListReducer } from '../SearchResults/reducer';
+import { SEARCH_RESULT_CLAIM_PUBLICATION } from '../../constants/search';
 
 const RecordState = {
     clear: {
@@ -36,57 +37,49 @@ const RecordState = {
     }
 };
 
-// Immutable state
-export const initialState = Immutable.fromJS({
-    claimPublicationResults: {},
-    loadingSearch: false,
-    selectedPublication: {},
-    recordClaimState: undefined,
-    recordClaimErrorMessage: undefined
-});
-
-const handlers = {
-
-    [PUBLICATION_SELECTED]: (state, action) => {
-        const entry = state.get('claimPublicationResults').find(entry => {
-            return entry.get('rek_pid') === action.payload;
-        });
-
-        return state.set('selectedPublication', Immutable.fromJS(entry));
-    },
-
-    [USERS_PUBLICATIONS_LOADING]: (state) => state.set('loadingSearch', true),
-
-    [PUBLICATION_RESULTS_CLEARED]: (state, action) => state.set('claimPublicationResults', Immutable.fromJS(action.payload)).set('loadingSearch', false),
-    [USERS_PUBLICATIONS_LOADED]: (state, action) => state.set('claimPublicationResults', Immutable.fromJS(action.payload)).set('loadingSearch', false),
-
-    [PUBLICATION_SELECTED_CLEARED]: (state, action) => state.set('selectedPublication', Immutable.fromJS(action.payload)),
-
-    [USER_PUBLICATIONS_MARKED_NOT_MINE_COMPLETED]: (state) => {
-        const updatedState = state.get('claimPublicationResults').filter((item, index) => {
-            return index >= locale.pages.claimPublications.maxSearchResults;
-        });
-        return state.set('claimPublicationResults', Immutable.fromJS(updatedState));
-    },
-
-    [CLAIM_SUBMITTED]: (state) => (state.set('recordClaimState', Immutable.fromJS(RecordState.submitted)).set('recordClaimErrorMessage', undefined)),
-
-    [CLAIM_SUBMIT_FAILED]: (state, action) => (state.set('recordClaimState', Immutable.fromJS(RecordState.failed)).set('recordClaimErrorMessage', Immutable.fromJS(action.payload))),
-
-    [CLAIM_SUBMITTING]: (state) => (state.set('recordClaimState', Immutable.fromJS(RecordState.submitting)).set('recordClaimErrorMessage', undefined)),
-
-    [CLAIM_SUBMIT_RESET]: (state) => (state.set('recordClaimState', Immutable.fromJS(RecordState.clear)).set('recordClaimErrorMessage', undefined)),
-
-    // TODO: refactor: rely on redux-form failed submission api, not on manual state reset
-    // reset failed submit state when user 'starts' to modify something on the form by moving focus
-    [actionTypes.FOCUS]: (state) => (state.set('recordClaimState', Immutable.fromJS(RecordState.clear)).set('recordClaimErrorMessage', undefined))
-};
-
-
-export default function claimPublicationsReducer(state = initialState, action) {
-    const handler = handlers[action.type];
-    if (!handler) {
-        return state;
+function selectedPublication(state = {}, action) {
+    switch (action.type) {
+        case PUBLICATION_SELECTED:
+            return state.get('claimPublication').get('searchResultsList').find(entry => {
+                return entry.get('rek_pid') === action.payload;
+            });
+        case PUBLICATION_SELECTED_CLEARED:
+            return {};
+        default:
+            return state;
     }
-    return handler(state, action);
 }
+
+function recordClaimState(state = {}, action) {
+    switch (action.type) {
+        case CLAIM_SUBMITTING:
+            return RecordState.submitting;
+        case CLAIM_SUBMITTED:
+            return RecordState.submitted;
+        case CLAIM_SUBMIT_FAILED:
+            return RecordState.failed;
+        case CLAIM_SUBMIT_RESET:
+            return RecordState.clear;
+        default:
+            return state;
+    }
+}
+
+function recordClaimErrorMessage(state = '', action) {
+    switch (action.type) {
+        case CLAIM_SUBMIT_FAILED:
+            return action.payload;
+        case CLAIM_SUBMITTING:
+        case CLAIM_SUBMITTED:
+        case CLAIM_SUBMIT_RESET:
+        default:
+            return state;
+    }
+}
+
+export default combineReducers({
+    searchResultsList: createSearchResultsListReducer(SEARCH_RESULT_CLAIM_PUBLICATION),
+    selectedPublication,
+    recordClaimState,
+    recordClaimErrorMessage
+});
