8907efee3bee429ec9273cb225ca3f2379861026
Vishal Asai <asai_vishal@yahoo.com>
2017-07-20 11:51:16 +1000

    Update search results reducer

--- src/modules/SearchResults/reducer.js
+++ src/modules/SearchResults/reducer.js
@@ -3,29 +3,19 @@
  */
 import { combineReducers } from 'redux-immutable';
 import {
-    SEARCH_INITIATED,
     SEARCH_LOADING,
     SEARCH_COMPLETED,
     CLEAR_SEARCH_RESULTS,
     INTERNAL_SEARCH_LOADING, WOS_SEARCH_LOADING, SCOPUS_SEARCH_LOADING, CROSSREF_SEARCH_LOADING, PUBMED_SEARCH_LOADING,
     INTERNAL_SEARCH_COMPLETED, WOS_SEARCH_COMPLETED, SCOPUS_SEARCH_COMPLETED, CROSSREF_SEARCH_COMPLETED, PUBMED_SEARCH_COMPLETED,
-    INTERNAL_SEARCH_FAILED, WOS_SEARCH_FAILED, SCOPUS_SEARCH_FAILED, CROSSREF_SEARCH_FAILED, PUBMED_SEARCH_FAILED
-} from './constants';
-import { convertToQueryString } from '../../helpers/search';
-
-function searchPublicationText(state = '', action) {
-    switch (action.type) {
-        case SEARCH_INITIATED:
-            return convertToQueryString(action.payload);
-        default:
-            return state;
-    }
-}
+    INTERNAL_SEARCH_FAILED, WOS_SEARCH_FAILED, SCOPUS_SEARCH_FAILED, CROSSREF_SEARCH_FAILED, PUBMED_SEARCH_FAILED,
+} from '../../constants/search';
 
 function loadingSearch(state = false, action) {
     switch (action.type) {
         case SEARCH_LOADING:
             return true;
+        case SEARCH_COMPLETED:
         case INTERNAL_SEARCH_COMPLETED:
         case WOS_SEARCH_COMPLETED:
         case SCOPUS_SEARCH_COMPLETED:
@@ -54,6 +44,7 @@ function loadingMoreSearch(state = false, action) {
 
 function searchResultsList(state = [], action) {
     switch (action.type) {
+        case SEARCH_COMPLETED:
         case INTERNAL_SEARCH_COMPLETED:
         case WOS_SEARCH_COMPLETED:
         case SCOPUS_SEARCH_COMPLETED:
@@ -72,11 +63,19 @@ function searchResultsList(state = [], action) {
     }
 }
 
+export function createSearchResultsListReducer(searchResultFor) {
+    return (state, action) => {
+        const { name } = action;
+        const isInitializationCall = state === undefined;
+        if(name !== searchResultFor && !isInitializationCall) return state;
+
+        return searchResultsList(state, action);
+    };
+}
+
 const searchResultReducer = combineReducers({
-    searchPublicationText,
     loadingSearch,
-    loadingMoreSearch,
-    searchResultsList
+    loadingMoreSearch
 });
 
 export default searchResultReducer;
