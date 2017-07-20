6a4f876a79e397a9d66e1263c5540af7c8478246
Vishal Asai <asai_vishal@yahoo.com>
2017-07-20 11:50:13 +1000

    Stop using Immutable and simplify add record reducer

--- src/modules/AddRecord/reducer.js
+++ src/modules/AddRecord/reducer.js
@@ -1,27 +1,26 @@
-import Immutable from 'immutable';
-
+import { combineReducers } from 'redux-immutable';
+import { createSearchResultsListReducer } from '../SearchResults/reducer';
+import { SEARCH_RESULT_ADD_RECORD } from '../../constants/search';
 import {
     ADD_RECORD_STEPPER_INDEX_INCREASED,
     ADD_RECORD_STEPPER_INDEX_DECREASED,
     ADD_RECORD_STEPPER_INDEX_RESET
 } from './actions';
 
-// Immutable state
-export const initialState = Immutable.fromJS({
-    stepperIndex: 0
-});
-
-const addRecordReducer = (state = initialState, action) => {
+function stepperIndex(state = 0, action) {
     switch (action.type) {
         case ADD_RECORD_STEPPER_INDEX_INCREASED:
-            return state.set('stepperIndex', state.get('stepperIndex') + 1);
+            return state + 1;
         case ADD_RECORD_STEPPER_INDEX_DECREASED:
-            return state.set('stepperIndex', state.get('stepperIndex') - 1);
+            return state - 1;
         case ADD_RECORD_STEPPER_INDEX_RESET:
-            return state.set('stepperIndex', 0);
+            return 0;
         default:
             return state;
     }
-};
+}
 
-export default addRecordReducer;
+export default combineReducers({
+    stepperIndex,
+    searchResultsList: createSearchResultsListReducer(SEARCH_RESULT_ADD_RECORD)
+});
